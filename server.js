5fbf52a9de97345d5d05aeb9f6f0f6874c6c5b02
Krishmon Karki <kissmoon17@gmail.com>
2026-09-20 18:56:12 +0545

    Add CORS headers to enable dashboard access

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ require('dotenv').config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+console.log('CORS headers set');
 // Middleware: parse JSON from requests
 // Why? If someone sends JSON data, we need to read it
 app.use(express.json());
