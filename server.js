58e19a97a129f46c2e94a1a6d3b0089943e8b97e
Krishmon Karki <kissmoon17@gmail.com>
2026-09-21 01:15:49 +0545

    Fix static file serving from public folder

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
-app.use(express.static(__dirname));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
